265406737f5afad86a565a9462e9b7d1a9fcc6cd
David Kossoglyad <david@logoui.co.il>
2016-12-05 23:12:12 +0200

    Intermediate commit

---
+++ src/app/shared/nav-bar/nav-bar.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { NavBarComponent } from './nav-bar.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        NavBarComponent,
+    ],
+    exports: [
+        NavBarComponent,
+    ]
+})
+export class NavBarModule {
+
+}
